f95e87efb0c76ada36928ebd08ae2daf3742010c
Dev <kanuj2151@gmail.com>
2023-11-04 23:14:26 +0530

    5.2

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -14,8 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
+
 
 
 @RestController
